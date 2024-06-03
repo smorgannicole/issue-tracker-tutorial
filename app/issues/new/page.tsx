fccfdb9d1539fc0992a2f5539958f890a617af3a
Morgan <smorgannicole@gmail.com>
2024-06-03 17:54:47 -0500

    built new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+import { Button, TextField } from "@radix-ui/themes";
+import { TextArea } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title"></TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,16 @@
+"use client";
 import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
