b950559eb2ad5cbcf4763deba4215500055ba49f
Morgan <smorgannicole@gmail.com>
2024-06-04 15:37:54 -0500

    errors handled

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
@@ -13,60 +14,52 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <div>
-        <label
-          htmlFor="title"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Title
-        </label>
-        <TextField.Root
-          id="title"
-          {...register("title", { required: "Title is required" })}
-        ></TextField.Root>
-        {errors.title && (
-          <span role="alert" className="text-red-500 text-sm">
-            {errors.title.message}
-          </span>
-        )}
-      </div>
-      <div>
-        <label
-          htmlFor="description"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Description
-        </label>
-        <Controller
-          name="description"
-          control={control}
-          rules={{ required: "Description is required" }}
-          render={({ field }) => <SimpleMDE id="description" {...field} />}
-        />
-        {errors.description && (
-          <span role="alert" className="text-red-500 text-sm">
-            {errors.description.message}
-          </span>
-        )}
-      </div>
-
-      <Button type="submit">Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Title
+          </label>
+          <TextField.Root id="title" {...register("title")}></TextField.Root>
+        </div>
+        <div>
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Description
+          </label>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => <SimpleMDE id="description" {...field} />}
+          />
+        </div>
+        <Button type="submit">Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
