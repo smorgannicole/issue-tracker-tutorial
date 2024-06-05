f09ac84d260757b1c63a8481065a6d17db0f3e00
Morgan <smorgannicole@gmail.com>
2024-06-05 18:57:16 -0500

    404 issue resolved

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,8 +7,6 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") notFound();
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
