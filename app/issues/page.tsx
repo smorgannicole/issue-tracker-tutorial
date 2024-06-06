c1d802bdb69b8d7b66614d020d9014d4e8216eb4
Morgan <smorgannicole@gmail.com>
2024-06-06 12:39:04 -0500

    styled link component and refactored

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
+import Link from "../components/Link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+  await delay(1000);
 
   return (
     <div>
