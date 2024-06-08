ef1182a59545a9caf165042b09b4bf2cdb5a5eb4
Morgan <smorgannicole@gmail.com>
2024-06-08 10:50:08 -0500

    built API for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,38 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // 1st parameter: request is the incoming http request of type NextRequest
+  // NextRequest is a predefined type in Next.js- it represents http request object received by the server
+
+  // 2nd parameter: 2 things happening- destructuring and type annotation
+  // fxn is expecting obj as 2nd argument and will extract params property from this obj
+  // id property inside params is of type string
+
+  const body = await request.json();
+  // once the server responds, body of the http request is parsed as json
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
