c680116da1fb3e75eee469a416d60bf2ffa09b4f
Morgan <smorgannicole@gmail.com>
2024-06-10 11:42:34 -0500

    improve loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,21 +1,22 @@
 "use client";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
-import { useForm, Controller } from "react-hook-form";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { issueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { issueSchema } from "@/app/validationSchemas";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import { Issue } from "@prisma/client";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+// const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+//   ssr: false,
+// });
+// no longer need since entire form is being imported dynamically elsewhere
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,14 @@
-import IssueForm from "../_components/IssueForm";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
+// 1st arg is loader fxn, 2nd is
+// dynamically importing the IssueForm component so both fields load simultaneously rather than staggered
+// code-splitting- the component will only be loaded when it's actually needed. improves performance of app
+// component only to be rendered on the client side
 
 const NewIssuePage = () => {
   return <IssueForm />;
