4e4cc340b29336bd2413f9d92538795195faf693
Morgan <smorgannicole@gmail.com>
2024-06-10 18:09:57 -0500

    handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,45 +3,70 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [deleteError, setDeleteError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setDeleteError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      {/* serves as container for the dialog and manages state (open or closed) */}
-      <AlertDialog.Trigger>
-        {/* trigger for opening the dialog and wraps around delete btn component- when btn is clicked, it triggers the AlertDialog to open */}
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        {/* defines content of the dialog that will be displayed when the dialog is opened */}
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action cannot be
-          undone.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="3">
-          <AlertDialog.Cancel>
-            <Button color="gray" variant="soft">
-              {/* variant flavors the component's color. "soft" lightens the standalone gray */}
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                await axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-            >
-              Delete Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        {/* serves as container for the dialog and manages state (open or closed) */}
+        <AlertDialog.Trigger>
+          {/* trigger for opening the dialog and wraps around delete btn component- when btn is clicked, it triggers the AlertDialog to open */}
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          {/* defines content of the dialog that will be displayed when the dialog is opened */}
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft">
+                {/* variant flavors the component's color. "soft" lightens the standalone gray */}
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteIssue}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={deleteError}>
+        {/* this root has a prop called open that can be set t/f */}
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            variant="soft"
+            color="gray"
+            mt="2"
+            onClick={() => setDeleteError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
